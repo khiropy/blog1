5b7912636ba7a1b857967cf0dc3068a39cde778c
khiropy <khiro5151@gmail.com>
2021-06-01 14:12:37 +0100

    forms and user accounts

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
+from django.urls import reverse
 
 class Post(models.Model):
-    title = models.TextField(max_length=200)
+    title = models.TextField(max_length=20)
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     body = models.TextField()
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail',args=[str(self.id)])
+
 
 
 # Create your models here.
